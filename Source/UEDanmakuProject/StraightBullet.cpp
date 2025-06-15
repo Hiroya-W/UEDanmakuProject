e8f0f4e25a4e0f2fd74158de28c2f4370ede1fe2
Hiroya-W <hyuyukun@gmail.com>
2025-06-16 00:07:59 +0900

    :sparkles: 真っすぐ進むだけの弾を作成

--- Source/UEDanmakuProject/StraightBullet.cpp
+++ Source/UEDanmakuProject/StraightBullet.cpp
@@ -3,25 +3,50 @@
 
 #include "StraightBullet.h"
 
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
 // Sets default values
 AStraightBullet::AStraightBullet()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionComponent"));
+	RootComponent = CollisionComponent;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAssset(
+		TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAssset.Succeeded())
+	{
+		MeshComponent->SetStaticMesh(SphereVisualAssset.Object);
+		MeshComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+		MeshComponent->SetWorldScale3D(FVector(0.5f));
+	}
+
+	ProjectileMovementComponent =
+		CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent->SetUpdatedComponent(RootComponent);
+	ProjectileMovementComponent->InitialSpeed = 300.0f;
+	ProjectileMovementComponent->MaxSpeed = 300.0f;
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+	ProjectileMovementComponent->bShouldBounce = false;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+
+	InitialLifeSpan = 3.0f;
 }
 
 // Called when the game starts or when spawned
 void AStraightBullet::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AStraightBullet::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
-
--- Source/UEDanmakuProject/StraightBullet.h
+++ Source/UEDanmakuProject/StraightBullet.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+
 #include "StraightBullet.generated.h"
+
+class USphereComponent;
+class UStaticMeshComponent;
+class UProjectileMovementComponent;
 
 UCLASS()
 class UEDANMAKUPROJECT_API AStraightBullet : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AStraightBullet();
 
@@ -19,8 +24,16 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* CollisionComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UProjectileMovementComponent* ProjectileMovementComponent;
 };
