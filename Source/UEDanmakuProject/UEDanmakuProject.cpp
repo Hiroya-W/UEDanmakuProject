df01687baa8ebdd0d131ed46d9085f5c5ae1b60e
Hiroya-W <hyuyukun@gmail.com>
2025-06-15 23:37:01 +0900

    :recycle: format

--- Source/UEDanmakuProject/UEDanmakuProject.cpp
+++ Source/UEDanmakuProject/UEDanmakuProject.cpp
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UEDanmakuProject.h"
+
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEDanmakuProject, "UEDanmakuProject" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, UEDanmakuProject, "UEDanmakuProject");
