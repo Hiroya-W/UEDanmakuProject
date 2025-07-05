84955681423635f8dea5c0eff3386b64ac3d99a2
Hiroya-W <hyuyukun@gmail.com>
2025-07-05 18:28:52 +0900

    :sparkles: 弾幕の差し替えができるようにBaseクラスを定義

---
+++ Source/UEDanmakuProject/Public/Components/FireStrategyBase.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+UCLASS(Abstract)
+class UEDanmakuProject_API UFireStrategyBase : public UActorComponent
+{
+public:
+	GENERATED_BODY()
+	// 発射ロジック
+	virtual void Fire() PURE_VIRTUAL(UFireStrategyBase::Fire);
+
+	// 発射間隔を返す
+	// 現状このクラスのインスタンスを持つActorでタイマーを設定しているので取得できるようにしている
+	virtual float GetShotRate() PURE_VIRTUAL(UFireStrategyBase::GetShotRate, return 0.0f;);
+};
