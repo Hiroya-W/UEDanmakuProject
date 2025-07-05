05c2c28f7176c4b5564892cba744f35c360eecd3
Hiroya-W <hyuyukun@gmail.com>
2025-07-05 21:42:06 +0900

    :art: BulletEmitterに見た目がついていなかった

--- Source/UEDanmakuProject/Private/Pawns/BulletEmitterComponentBase.cpp
+++ Source/UEDanmakuProject/Private/Pawns/BulletEmitterComponentBase.cpp
@@ -4,13 +4,25 @@
 #include "Pawns/BulletEmitterComponentBase.h"
 
 #include "Components/NoFireStrategy.h"
-#include "Components/SpiralFireStrategy.h"
 
 // Sets default values
 ABulletEmitterComponentBase::ABulletEmitterComponentBase()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	// 自分の見た目を立方体として表現しておく
+	// TODO:  いい感じになおす
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	RootComponent = MeshComponent;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(
+		TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+	if (CubeVisualAsset.Succeeded())
+	{
+		MeshComponent->SetStaticMesh(CubeVisualAsset.Object);
+		MeshComponent->SetWorldScale3D(FVector(0.5f));
+	}
+
 	// デフォルト値として弾幕を撃たない設定を利用する
 	FireStrategy = CreateDefaultSubobject<UNoFireStrategy>(TEXT("FireStrategy"));
 }
